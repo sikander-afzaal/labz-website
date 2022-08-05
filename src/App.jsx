b1178654a112249a3c1ea6e9848a78ea2622435d
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-08-05 18:54:36 +0500

    first

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     );
     const section1Timeline = gsap.timeline();
     section1Timeline
-      .to(document.querySelectorAll(".first-section h1"), {
+      .to(document.querySelectorAll(".first-h1"), {
         opacity: 1,
         y: 0,
         rotateX: 0,
@@ -26,6 +26,7 @@ function App() {
         z: 0,
         ease: Sine.easeOut,
       })
+      .to(document.querySelector(".text-row"), { opacity: 1, y: 0 })
       .to(firstSectionBeforeRule, {
         ease: Expo.easeOut,
         duration: 2,
@@ -36,10 +37,25 @@ function App() {
     <div className="App">
       <Header />
       <div className="first-section">
-        <h1>ENGINEERING </h1>
-        <h1>AESTHETICS</h1>
-        <h1>OF DIGITAL</h1>
-        <h1>PRODUCTS</h1>
+        <h1 className="first-h1">ENGINEERING </h1>
+        <h1 className="first-h1">AESTHETICS</h1>
+        <h1 className="first-h1">OF DIGITAL</h1>
+        <div className="text-part">
+          <h1 className="first-h1">Products</h1>
+          <div className="text-row">
+            <div className="col">
+              <p>
+                ARTEM MARKOVSKY <br /> INDEPENDENT ART DIRECTOR <br /> &
+                MULTIDISCIPLINARY DESIGNER
+              </p>
+            </div>
+            <div className="col">
+              <p>
+                BRANDING & UX <br /> DESIGN BOUTIQUE
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
